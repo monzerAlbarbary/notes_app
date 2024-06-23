cb97eec5efcfbafbfdd95725d7ae4c37c85866c2
monzerAB <monzer.albar@gmail.com>
2024-06-23 13:23:43 +0300

    create notesApp and change theme and greate notes view

---
+++ lib/views/notes_view.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/material.dart';
+
+class NotesViews extends StatelessWidget {
+
+  const NotesViews({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
